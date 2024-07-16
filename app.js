5ba3263352083ee23856ed179348ca8725cf3974
Addo Michael <michaeladdo1997@gmail.com>
2024-07-16 23:05:33 +0000

    created my folders

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const cookieparser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const cors = require("cors");
 const userRouter = require("./routes/userRoutes");
 const examRouter = require("./routes/examRoute");
 
+app.use(cors());
+app.options("*", cors());
 app.use(express.json());
+app.use(cookieparser());
+app.use(mongoSanitize());
 
 ///Mounting routers
+
 // app.use("/api/v1/users", userRouter);
 // app.use("/api/v1/exams", examRouter);
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB, {}).then(() => console.log("DB connected successful"));
 const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
