3e83519c62a897491693451710fc7ea3b9b6f2f3
Addo Michael <michaeladdo1997@gmail.com>
2024-08-20 21:23:34 +0000

    .

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 mongoose.connect(DB, {}).then(() => console.log("DB connected successful"));
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
